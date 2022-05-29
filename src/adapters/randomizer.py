3119db8140930dd0bf72a3c9f2786cd2bf3afa0c
erik <eriktyryshkin@gmail.com>
2022-05-29 01:31:10 -0700

    added support for a lower probability during night time and added template env file

--- src/adapters/randomizer.py
+++ src/adapters/randomizer.py
@@ -1,11 +1,22 @@
 import random
+from datetime import datetime, time
+
 
 class Randomizer:
     def __init__(self, probability: float):
         self.probability = probability
+
+        # night mode
+        if self.is_night():
+            self.probability = 0.1
 
     def randomize(self, func, *arg):
         if random.random() < self.probability:
             func(*arg)
             return True
         return False
+
+    def is_night(self):
+        now_time = datetime.now().time()
+        is_day = time(8, 00) <= now_time >= time(18, 00)
+        return not is_day
--- src/main.py
+++ src/main.py
@@ -14,37 +14,38 @@ load_dotenv()
 
 
 def get_recipient_numbers(filename):
-    with open(filename, 'r') as fd:
+    with open(filename, "r") as fd:
         contents = [n.strip() for n in fd.readlines()]
         return contents
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Stove.')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Stove.")
     parser.add_argument(
-        '-r',
-        '--recipients-file',
+        "-r",
+        "--recipients-file",
         required=True,
-        help='text file containing possible recipients',
+        help="text file containing possible recipients",
         type=str,
     )
     parser.add_argument(
-        '-d',
-        '--database-file',
-        help='csv file contain sources for sending text messages',
+        "-d",
+        "--database-file",
+        help="csv file contain sources for sending text messages",
         type=str,
     )
     parser.add_argument(
-        '-p',
-        '--message-probability',
-        help='probability for sending a text',
-        type=float, default=0.25
+        "-p",
+        "--message-probability",
+        help="probability for sending a text",
+        type=float,
+        default=0.25,
     )
     args = parser.parse_args()
 
     text_generator = TextGenerator(args.database_file)
     account_store = AccountStore(
-        os.environ['STOVE_GVMS_HOSTNAME'], os.environ['STOVE_GVMS_PORT']
+        os.environ["STOVE_GVMS_HOSTNAME"], os.environ["STOVE_GVMS_PORT"]
     )
 
     randomizer = Randomizer(args.message_probability)
@@ -59,4 +60,4 @@ if __name__ == '__main__':
                 lambda: account.send_text(text_generator.randomize(), recipient)
             )
             if ok:
-                print('Account', account.get_number(), '|', 'Recipient', recipient)
+                print("Account", account.get_number(), "|", "Recipient", recipient)
