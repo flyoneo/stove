59ad38ed0b2b397427b2764a273a76fe44d500fd
erik <eriktyryshkin@gmail.com>
2022-05-29 00:09:42 -0700

    added gvms intergation and accounts store for managing gvms accounts

---
+++ src/accounts/account.py
@@ -0,0 +1,16 @@
+import accounts.v1.gvoice_pb2
+
+
+class Account:
+    def __init__(self, client, number):
+        self._client = client
+        self._number = number
+
+    def send_text(self, text: str, recipient_number: str):
+        self._client.SendSMS(
+            accounts.v1.gvoice_pb2.SendSMSRequest(
+                gvoice_phone_number=self._number,
+                recipient_phone_number=recipient_number,
+                message=text,
+            )
+        )
---
+++ src/accounts/account_store.py
@@ -0,0 +1,35 @@
+import grpc
+
+from accounts.account import Account
+import accounts.v1.gvoice_pb2
+import accounts.v1.gvoice_pb2_grpc
+
+
+class AccountStore:
+    def __init__(self, hostname, port):
+        channel = grpc.insecure_channel(f"{hostname}:{port}")
+        self._client = accounts.v1.gvoice_pb2_grpc.GVoiceStub(channel)
+        self._accounts = {}
+
+    def setup(self):
+        numbers = self._get_numbers()
+        for number in numbers:
+            self._accounts[number] = Account(self._client, number)
+
+    def get_account(self, number):
+        if number not in self._accounts:
+            raise ValueError(f"No account exists for number {number}.")
+
+        return self._accounts[number]
+
+    def get_accounts(self):
+        return self._accounts.keys()
+
+    def _get_numbers(self):
+        resp = self._client.GetGVoiceNumbers(
+            accounts.v1.gvoice_pb2.FetchGVoiceNumbersRequest()
+        )
+        if not resp.success:
+            raise ValueError("Unable to fetch GVoice numbers from GVMS.")
+
+        return resp.gvoice_phone_numbers
